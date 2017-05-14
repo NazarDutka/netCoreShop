b0a0c7f570c339755b9b8b94d04b882d2ed02ab5
Nazar <dutka.nazar@gmail.com>
2017-05-14 20:59:55 +0300

    addModels

---
+++ src/netCoreShop/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netCoreShop.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace netCoreShop.Controllers
+{
+    public class PieController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IPieRepository _pieRepository;
+        public PieController(ICategoryRepository categoryRepository, IPieRepository pieRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+        public ViewResult List()
+        {
+            return View(_pieRepository.Pies);
+        }
+    }
+}
---
+++ src/netCoreShop/Models/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace netCoreShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> Pies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie GetPieById(int payId);
+    }
+}
